75ca84601c91fba4c8b50badb30ddf394d5bf510
ZhuoFuJiang <1136634662@qq.com>
2023-07-08 14:26:10 +0800

    add xgboost algorithm

--- supervised_learning/decision_tree.py
+++ supervised_learning/decision_tree.py
@@ -176,7 +176,7 @@ class DecisionTree(object):
 
         # If we're at leaf => print the label
         if tree.value is not None:
-            print (tree.value)
+            print(tree.value)
         # Go deeper down the tree
         else:
             # Print test
@@ -194,6 +194,7 @@ class XGBoostRegressionTree(DecisionTree):
     Regression tree for XGBoost
     - Reference -
     http://xgboost.readthedocs.io/en/latest/model.html
+    该实现未加正则项
     """
 
     def _split(self, y):
@@ -204,8 +205,8 @@ class XGBoostRegressionTree(DecisionTree):
         return y, y_pred
 
     def _gain(self, y, y_pred):
-        nominator = np.power((y * self.loss.gradient(y, y_pred)).sum(), 2)
-        denominator = self.loss.hess(y, y_pred).sum()
+        nominator = np.power((np.sum(self.loss.gradient(y, y_pred), axis=0)), 2)
+        denominator = np.sum(self.loss.hess(y, y_pred), axis=0)
         return 0.5 * (nominator / denominator)
 
     def _gain_by_taylor(self, y, y1, y2):
@@ -217,22 +218,67 @@ class XGBoostRegressionTree(DecisionTree):
         true_gain = self._gain(y1, y1_pred)
         false_gain = self._gain(y2, y2_pred)
         gain = self._gain(y, y_pred)
-        return true_gain + false_gain - gain
+        return sum(true_gain + false_gain - gain)
 
     def _approximate_update(self, y):
         # y split into y, y_pred
         y, y_pred = self._split(y)
         # Newton's Method
-        gradient = np.sum(y * self.loss.gradient(y, y_pred), axis=0)
+        gradient = np.sum(self.loss.gradient(y, y_pred), axis=0)
         hessian = np.sum(self.loss.hess(y, y_pred), axis=0)
-        update_approximation =  gradient / hessian
-
+        update_approximation = gradient / hessian
         return update_approximation
 
     def fit(self, X, y):
         self._impurity_calculation = self._gain_by_taylor
         self._leaf_value_calculation = self._approximate_update
         super(XGBoostRegressionTree, self).fit(X, y)
+
+
+# class XGBoostRegressionTree(DecisionTree):
+#     """
+#     Regression tree for XGBoost
+#     - Reference -
+#     http://xgboost.readthedocs.io/en/latest/model.html
+#     """
+#
+#     def _split(self, y):
+#         """ y contains y_true in left half of the middle column and
+#         y_pred in the right half. Split and return the two matrices """
+#         col = int(np.shape(y)[1]/2)
+#         y, y_pred = y[:, :col], y[:, col:]
+#         return y, y_pred
+#
+#     def _gain(self, y, y_pred):
+#         nominator = np.power((y * self.loss.gradient(y, y_pred)).sum(), 2)
+#         denominator = self.loss.hess(y, y_pred).sum()
+#         return 0.5 * (nominator / denominator)
+#
+#     def _gain_by_taylor(self, y, y1, y2):
+#         # Split
+#         y, y_pred = self._split(y)
+#         y1, y1_pred = self._split(y1)
+#         y2, y2_pred = self._split(y2)
+#
+#         true_gain = self._gain(y1, y1_pred)
+#         false_gain = self._gain(y2, y2_pred)
+#         gain = self._gain(y, y_pred)
+#         return true_gain + false_gain - gain
+#
+#     def _approximate_update(self, y):
+#         # y split into y, y_pred
+#         y, y_pred = self._split(y)
+#         # Newton's Method
+#         gradient = np.sum(y * self.loss.gradient(y, y_pred), axis=0)
+#         hessian = np.sum(self.loss.hess(y, y_pred), axis=0)
+#         update_approximation = gradient / hessian
+#
+#         return update_approximation
+#
+#     def fit(self, X, y):
+#         self._impurity_calculation = self._gain_by_taylor
+#         self._leaf_value_calculation = self._approximate_update
+#         super(XGBoostRegressionTree, self).fit(X, y)
 
 
 class RegressionTree(DecisionTree):
